db0cf4e727783a85a5c557beb2063df59a6e4328
Derz65 <lm1030921@gmail.com>
2025-04-21 22:12:26 +0300

    main

---
+++ Task3SqlServer/Core/CoreConnection.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows.Controls;
+using Task3SqlServer.Model;
+
+public static class CoreConnection
+{
+    public static Frame CoreFrame { get; set; }
+    public static Task3Entities DB { get; set; }
+}
